8100c5f6483ce6953e52d2c6d916aacfbddc8ab3
Miten <mbhadania@smu.edu>
2016-02-29 15:27:45 -0500

    backend final commit

--- src/main/java/com/springboot/controller/GuestbookController.java
+++ src/main/java/com/springboot/controller/GuestbookController.java
@@ -29,11 +29,7 @@ public class GuestbookController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     String list(Model model, HttpServletRequest request) {
-        // Query query = new Query("Greeting").addSort("date", Query.SortDirection.DESCENDING);
-        // List<Entity> greetings = datastoreService.prepare(query).asList(FetchOptions.Builder.withLimit(30));
-        // model.addAttribute("greetings", greetings);
-
-        //System.out.println("\n\n\n\n " + thcheck + "\n\n\n\n\n\n\n\n\n\n");
+        
         User user = userService.getCurrentUser();
         if (user != null) {
             String username = user.getEmail();
@@ -45,8 +41,8 @@ public class GuestbookController {
         return "list";
     }
 
+    // FInding combinations
     ArrayList<String> letterCombinations = new ArrayList<String>();
-
     @RequestMapping(value = "/", method = RequestMethod.POST)
     String combos(@RequestParam("number") long number, Model model) {
 
@@ -57,32 +53,22 @@ public class GuestbookController {
         } catch (Exception e) {
             System.out.println(e);
         }
-        //model.addAttribute("letterCombinations", letterCombinations);
         model.addAttribute("totalCombos", letterCombinations.size());
         model.addAttribute("totalPages", letterCombinations.size() / 50 + 1);
-        return "list";
-        // return new OutputService(letterCombinations, letterCombinations.size(), letterCombinations.size()/50+1);
+        return "list";   
     }
 
+    // To service Ajax: REST Controller 
+    // Single page contains 50 combinations.
     @RequestMapping(value = "/page/{no}")
     @ResponseBody
-    OutputService combos1(@RequestParam("number") long number, @PathVariable int no) {
-
-        //System.out.println("\n\n\n\n " + number + "\n\n\n\n\n\n  pathvariable: " + no + "\n\n\n\n");
+    OutputService combos1(@RequestParam("number") long number, @PathVariable int no) { 
+        
         int pageSize = no * 50;
-        //Solution solution = new Solution();
-        // check check
-        //model.addAttribute("letterCombinations", letterCombinations);
         ArrayList<String> pageOutputList = new ArrayList<String>();
         for (int i = pageSize - 50; i < pageSize && i < letterCombinations.size(); i++) {
             pageOutputList.add(letterCombinations.get(i));
         }
-
-        //  for(int i=no*50;i>(no-1)*50;i--){
-        //     pageOutputList.add(letterCombinations.get(i));
-        // }
         return new OutputService(pageOutputList, letterCombinations.size(), letterCombinations.size() / 50 + 1);
-        //return new OutputService(letterCombinations, letterCombinations.size(), letterCombinations.size()/50+1);
     }
-
 }
--- src/main/java/com/springboot/controller/OutputService.java
+++ src/main/java/com/springboot/controller/OutputService.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package com.springboot.controller;
-
 import java.util.ArrayList;
 
 /**
@@ -39,7 +38,5 @@ public class OutputService {
     @Override
     public String toString() {
         return "OutputService{" + "combination=" + combination + ", totalCombos=" + totalCombos + ", totalPages=" + totalPages + '}';
-    }
-
-    
+    } 
 }
